3b37756ba97359d57025aede68d5c447b2d54365
Andrew <Andrew@DESKTOP-4E5PJBO>
2018-07-31 20:30:02 -0300

    Criando Hello World

--- Aulas/Aula3107_console/Program.cs
+++ Aulas/Aula3107_console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula3107_console
 {
@@ -10,6 +6,9 @@ namespace Aula3107_console
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey();
         }
     }
 }
