6157d96eff347a3b041838a7b16aec9e99859b6c
Andrew <Andrew@DESKTOP-4E5PJBO>
2018-08-14 21:41:13 -0300

    Instalando Entity framework

--- Aulas/Aula0708_WPF/MainWindow.xaml.cs
+++ Aulas/Aula0708_WPF/MainWindow.xaml.cs
@@ -20,9 +20,200 @@ namespace Aula0708_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string operacao;
+        double x;
+        double y;
+        double pot;
+        double? memoria;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+
+            txtTela.Text += "7";
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "8";
+
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "9";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "4";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "6";
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "3";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += "0";
+
+        }
+
+
+        private void btnIgual_Click(object sender, RoutedEventArgs e)
+        {
+            if (operacao == "+")
+            {
+
+                x += double.Parse(txtTela.Text);
+                txtTela.Text = x.ToString();
+            }
+            else if (operacao == "/")
+            {
+                if (double.Parse(txtTela.Text) != 0)
+                {
+                    x /= double.Parse(txtTela.Text);
+                    txtTela.Text = x.ToString();
+                }
+                else
+                {
+                    txtTela.Text = "Dividindo por zero";
+                }
+            }
+            else if (operacao == "-")
+            {
+                x -= double.Parse(txtTela.Text);
+                txtTela.Text = x.ToString();
+
+            }
+            else if (operacao == "x")
+            {
+                x *= double.Parse(txtTela.Text);
+                txtTela.Text = x.ToString();
+            }
+            else if (operacao == "pot")
+            {
+                y = double.Parse(txtTela.Text);
+                pot = Math.Pow(x, y);
+                txtTela.Text = pot.ToString();
+
+
+            }
+            else if (operacao == "raiz")
+            {
+                x = Math.Sqrt(x);
+                txtTela.Text = x.ToString();
+
+
+            }
+
+
+        }
+
+        private void btnSoma_Click(object sender, RoutedEventArgs e)
+        {
+
+            x = double.Parse(txtTela.Text);
+            txtTela.Text = "";
+            operacao = "+";
+
+
+        }
+
+
+        private void btnDiv_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(txtTela.Text);
+            txtTela.Text = "";
+            operacao = "/";
+
+        }
+
+        private void btnSub_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(txtTela.Text);
+            txtTela.Text = "";
+            operacao = "-";
+
+        }
+
+        private void btnMulti_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(txtTela.Text);
+            txtTela.Text = "";
+            operacao = "x";
+        }
+
+        private void btnVirgula_Click(object sender, RoutedEventArgs e)
+        {
+            txtTela.Text += ",";
+        }
+
+        private void btnC_Click(object sender, RoutedEventArgs e)
+        {
+            x = 0;
+            txtTela.Text = "";
+            operacao = "";
+
+        }
+
+        private void btnRaiz_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(txtTela.Text);
+            txtTela.Text = "";
+            operacao = "raiz";
+        }
+
+
+
+
+        private void BtnMemo_Click(object sender, RoutedEventArgs e)
+        {
+            if (memoria.HasValue)
+            {
+                txtTela.Text = memoria.ToString();
+                memoria = null;
+            }
+            else
+            {
+                memoria = double.Parse(txtTela.Text);
+            }
+        }
+
+        private void btnPot_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(txtTela.Text);
+            txtTela.Text = "";
+            operacao = "pot";
         }
     }
 }
