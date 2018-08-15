14c7ded225bc944deda2871d027f24a4cdc10177
Andrew <Andrew@DESKTOP-4E5PJBO>
2018-08-14 22:12:44 -0300

    Criando relacionamento entre tabelas

--- Aulas/Aula1408_EntityFramework/Class1.cs
+++ Aulas/Aula1408_EntityFramework/Class1.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace Aula1408_EntityFramework
 {
+    //Conceitos
+
+    //Instalando EntityFramework
+
+    //Criando base (abordagem: Model First)
+
+
+
+
+
+
     public class Class1
     {
     }
