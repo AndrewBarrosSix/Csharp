6cf32341ec64621e62ca490829a5eb0b9c365430
Andrew <Andrew@DESKTOP-4E5PJBO>
2018-08-16 21:54:55 -0300

    Implementando metodos de CRUD.

--- Aulas/Aula1608_EFDatabaseFirst/Modelo.Designer.cs
+++ Aulas/Aula1608_EFDatabaseFirst/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\andre\Source\Repos\Csharp\Aulas\Aula1408_EntityFramework\MeuDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\andre\source\repos\Csharp\Aulas\Aula1608_EFDatabaseFirst\Modelo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
